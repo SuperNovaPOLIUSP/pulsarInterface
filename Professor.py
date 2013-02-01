78a86d98e4f17dbded7e2ad561b001860bea7360
Bernardo Camargos <ber.camargos@gmail.com>
2013-02-01 15:00:01 -0200

    Professor.py added

---
+++ Professor.py
@@ -0,0 +1,74 @@
+
+class Professor(object):
+
+    """
+     A professor
+
+    :version:
+    :author:
+    """
+
+    """ ATTRIBUTES
+
+     The professor's name.
+
+    name  (public)
+
+     Associated data base key.
+
+    idProfessor  (public)
+
+    """
+
+    def __init__(self, name):
+        """
+         Professor's name is the basic attribute for creating a professor in your data
+         base.
+
+        @param string name : The professor's name.
+        @return  :
+        @author
+        """
+        pass
+
+    def pickById(self, idProfessor):
+        """
+         Returns a single professor with the chosen ID.
+
+        @param int idProfessor : Associated data base key.
+        @return Professor :
+        @author
+        """
+        pass
+
+    def pickByName(self, name):
+        """
+         Returns a single professor with the chosen name.
+
+        @param string name : The professor's name
+        @return Professor :
+        @author
+        """
+        pass
+
+    def findName(self, name):
+        """
+         Returns a list of professors with the name that contains the chosen one.
+
+        @param string name : The professor's name.
+        @return  :
+        @author
+        """
+        pass
+
+    def store(self):
+        """
+         Creates or alters the professor's data in the data base.
+
+        @return  :
+        @author
+        """
+        pass
+
+
+

f61b2690c520bc6fda8582a399b6e5c7121ba155
Henrique Carvalho Silva <henrique.cvlhs@gmail.com>
2013-02-26 12:21:38 -0300

    Added new methods to the AnswerType class

--- AnswerType.py
+++ AnswerType.py
@@ -2,64 +2,114 @@
 
 from tools.MySQLConnection import MySQLConnection
 
-class AnswerType(object):
-  
+class AnswerTypeError(Exception):
     """
-     Class that represents a category of multiple choice answers that a question can
-     be related to (e. g. the answer type "hours" may refer to different ammounts of
-     hours specified by alternatives A to E).
+     Exception that reports errors during the execution of AnswerType class methods
   
     :version:
     :author:
     """
-  
+    pass
+
+class AnswerType(object):
+
+    """
+     Class that represents a category of multiple choice answers that a question can
+     be related to (e. g. the answer type "hours" may refer to different ammounts of
+     hours specified by alternatives A to E).
+
+    :version:
+    :author:
+    """
+
     """ ATTRIBUTES
-  
+
      Associated database key.
-  
-    idAnswerType  (public)
-  
+
+    idAnswerType  (private)
+
      A dictionary containing the meaning of the answers alternative choices in the
      form { 'A':'meaningA' , 'B':'meaningB' , ..., 'E':'meaningE'}
-  
-    alternativeMeaning  (public)
-  
+
+    alternativeMeaning  (private)
+
      The name of the AnswerType related to a category of answers (e.g. hours, frequency).
-  
-    name  (public)
-  
+
+    name  (private)
+
     """
-  
+
     def __init__(self, name, alternativeMeaning):
         """
          Constructor method.
          Name and meaning are the necessary data to create an AnswerType.
-  
-        @param string name : The name of  the AnswerType related to a category of answers (e.g. hours, frequency).
+
+        @param string name : The name of the AnswerType related to a category of answers (e.g. hours, frequency).
         @param string{} alternativeMeaning : A dictionary containing the meaning of the answers alternative choices in the form { 'A':'meaningA' , 'B':'meaningB' , ..., 'E':'meaningE'}
         @return  :
         @author
         """
+        self.setName(name)
+        self.setAlternativeMeaning(alternativeMeaning)
+
+    def __eq__(self, other):
+        """
+         Comparison method that returns True if two objects of the class AnswerType are
+         equal.
+
+        @param AnswerType other : Other object of the class AnswerType to be compared with a present object.
+        @return bool :
+        @author
+        """
+        pass
+
+    def setName(self, name):
+        """
+         Changes the name of the category related to the answer type.
+
+        @param string name : The name of the AnswerType related to a category of answers (e.g. hours, frequency).
+        @return  :
+        @author
+        """
         # verifies if name is a proper parameter
-        if isinstance(name, unicode):
+        if isinstance(name, (unicode, str)):
             self.name = name
-            # verifies if alternativeMeaning is a proper parameter
-            if isinstance(alternativeMeaning, dict):
-                if len(alternativeMeaning) is 5:
-                    if 'A' and 'B' and 'C' and 'D' and 'E' in alternativeMeaning.keys():
-                        self.alternativeMeaning = alternativeMeaning
+        else:
+            raise AnswerTypeError("Invalid name parameter. It must be of type str or unicode.")
+
+    def setAlternativeMeaning(self, alternativeMeaning):
+        """
+         Changes the set of alternative meanings of the answer type.
+
+        @param string{} alternativeMeaning : A dictionary containing the meaning of the answers alternative choices in the form { 'A':'meaningA' , 'B':'meaningB' , ..., 'E':'meaningE'}
+        @return  :
+        @author
+        """
+        # verifies if alternativeMeaning is a proper parameter
+        if isinstance(alternativeMeaning, dict):
+            if len(alternativeMeaning) is 5:
+                if 'A' and 'B' and 'C' and 'D' and 'E' in alternativeMeaning.keys():
+                    for meaning in alternativeMeaning.values():
+                        if not isinstance(meaning, unicode) and not isinstance(meaning, str):
+                            raise AnswerTypeError("Invalid meaning found.")
+                    self.alternativeMeaning = alternativeMeaning
+                else:
+                    raise AnswerTypeError("Invalid set of alternatives. The parameter alternativeMeaning must be a dictionary with five keys, each key being a leter from A to E.")
+            else:
+                raise AnswerTypeError("Invalid set of alternatives. The parameter alternativeMeaning must be a dictionary with five keys, each key being a leter from A to E.")
+        else:                    
+            raise AnswerTypeError("Invalid set of alternatives. The parameter alternativeMeaning must be a dictionary with five keys, each key being a leter from A to E.")
 
     @staticmethod
     def pickById(idAnswerType):
         """
          Returns an AnswerType object given an idAnswerType.
          
-  
+
         @param int idAnswerType : Associated database key.
         @return AnswerType :
         @author
         """
-
         # gets connection with the mysql database
         databaseConnection = MySQLConnection() 
 
@@ -76,7 +126,7 @@ class AnswerType(object):
         pickedAnswerType.idAnswerType = idAnswerType
 
         return pickedAnswerType
-  
+
     @staticmethod
     def find(**kwargs):
         """
@@ -96,13 +146,12 @@ class AnswerType(object):
          The parameters must be identified by their names when the method is called, and
          those which are strings must be followed by "_like" or by "_equal", in order to
          determine the kind of search to be done.
-         E.g. AnswerType.find(name_equal = "time")
-  
+         E. g. AnswerType.find(name_equal = "time")
+
         @param {} **kwargs : Dictionary of arguments to be used as parameters for the search.
-        @return AnswerType[] :
+        @return  :
         @author
         """
-
         # gets connection with the mysql database
         databaseConnection = MySQLConnection()
 
@@ -114,24 +163,48 @@ class AnswerType(object):
         for Id in foundAnswerTypeIds:
             foundAnswerTypes.append(AnswerType.pickById(Id[0]))
 
-        return foundAnswerTypes        
-  
+        return foundAnswerTypes
+
     def store(self):
         """
-         Changes object on table or adds it to database if an object is absent. Returns
-         True if object is stored and False if it fails.
-  
-        @return bool :
+         Changes object on table or adds it to database if an object is absent.
+
+        @return  :
         @author
         """
-        pass
-  
+        # gets connection with the mysql database
+        databaseConnection = MySQLConnection()
+        
+        try:
+            if AnswerType.find(name_equal )
+
+
+'''
+        # verifies if the database already has an object like the one to be stored
+        try:
+            # verifies if the database possesses instances of the object
+            if len(databaseConnection.execute("SELECT * FROM answerType WHERE idAnswerType = " + str(self.idAnswerType))) > 0:
+                # updates the database instance of the object
+                databaseConnection.execute("UPDATE answerType SET name = " + self.name + " WHERE idAnswerType = " + str(self.idAnswerType))
+                databaseConnection.execute("UPDATE answerType SET alternative = " + alternative + ", meaning = " + meaning + " WHERE idAnswerType = " + str(self.idAnswerType))
+
+                # validates the updates made in the database
+                # databaseConnection.commit()
+                return True
+
+            else: 
+                # if the query was successful and no result was returned, the object has an invalid database ID
+                raise MySQLQueryError("Invalid AnswerType database ID")
+        except:
+            # creates new instance, for this object, in the database
+            databaseConnection.execute("INSERT INTO answerType (name) VALUES (" + self.name)
+'''
+
     def delete(self):
         """
-         Deletes object from the database. Returns True if the object was successfully
-         deleted and False if it fails.
-  
-        @return bool :
+         Deletes object from the database.
+
+        @return  :
         @author
         """
         pass

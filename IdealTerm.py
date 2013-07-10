076434c9e8e707813d8068b0fe277ab1cd4515b8
Heitor Reis <marcheing@gmail.com>
2013-07-10 12:30:05 -0300

    First alterations on IdealTerm class, will be changed later

--- IdealTerm.py
+++ IdealTerm.py
@@ -16,7 +16,7 @@ class IdealTermError(Exception):
 class IdealTerm(object):
 
     """
-     It is the group of disciplines that belongs to a curriculum's term.
+     It is the group of disciplines that belongs to a cycle's term.
 
     :version:
     :author:
@@ -26,9 +26,9 @@ class IdealTerm(object):
 
      Associated database key.
 
-    idCurriculum  (private)
+    idCycle  (private)
 
-     The term (number of the curriculum's semester or  quadmester) of the relation.
+     The term (number of the cycle's semester or  quadmester) of the relation.
 
     term  (private)
 
@@ -46,11 +46,11 @@ class IdealTerm(object):
 
     """
 
-    def __init__(self, idCurriculum, term, startDate):
+    def __init__(self, idCycle, term, startDate, requisitionType, course):
         """
          Constructor method.
 
-        @param int idCurriculum : Associated data base key.
+        @param int idCycle : Associated data base key.
         @param int term : Term of the ideal term.
         @param string startDate : Starting date of this ideal term.
         @return  :
@@ -58,14 +58,20 @@ class IdealTerm(object):
         """
         cursor = MySQLConnection()              
         
-        if not cursor.execute('SELECT idCurriculum FROM curriculum  WHERE idCurriculum = ' + str(idCurriculum)):   
-            raise IdealTermError('idCurriculum must be in the database')
-        self.idCurriculum = idCurriculum
+        if not cursor.execute('SELECT idCycle FROM cycle WHERE idCycle = ' + str(idCycle)):   
+            raise IdealTermError('idCycle must be in the database')
+        if not startDate or not isinstance(startDate, (str,unicode)) or not checkDateString(startDate):
+            raise IdealTermError('Must provide a valid start date string in unicode')
+        if not requisitionType or not isinstance(requisitionType, (int, long)):
+            raise IdealTermError('Must provide a valid requisition type integer')
+        if not isinstance(course, Course) or Course.pickById(course.idCourse) != course:
+            raise IdealTermError('Must provide a valid course from the database')
+        self.idCycle = idCycle
         self.term = term
-        self.courses = None
-        self.requisitionType = None
+        self.course = course
+        self.requisitionType = requisitionType
         self.startDate = startDate
-        self.endDate = '0000-00-00'
+        self.setEndDate('0000-00-00')
 
     def setEndDate(self, endDate):
         """
@@ -74,73 +80,10 @@ class IdealTerm(object):
         @param string endDate : String representing this IdealTerm's end date .
         @author
         """
+        if not isinstance(endDate, (str,unicode)) or not checkDateString(endDate):
+            raise IdealTermError('endDate parameter must be a valid string representing a date')
         self.endDate = endDate
 	
-    def addCourses(self, courses):
-        """
-             Adds courses to a relation.
-             Return:
-    
-             @param Course[] courses : Disciplines's list to be added to the relation.
-             @return bool :
-             @author
-        """
-        
-        cursor = MySQLConnection()
-        
-        try:
-            for course_data in courses:
-                # if the object in the list 'courses' is not an object neither is in the database, the parameter 'courses' must not be added to the idealTerm object
-                if not isinstance(course_data, Course):
-                        if not Course.pickById(course.idCourse) == course:
-                                raise CourseError('Course not in the database')
-        except:
-            raise IdealTermError("The parameter 'courses' must be a list of 'Course' objects.")
-
-        if not self.courses:
-            self.courses = []
-            for course in courses:
-                self.courses.append(course)
-        else:
-            for course_data in courses:
-                if not course_data in self.courses:
-                    self.courses.append(course_data)
-
-    def removeCourse(self, course):
-        """
-         Removes a course from its list.
-         Return: True if successful or False, otherwise
-
-        @param Course course : Course to be removed.
-
-        @return bool :
-        @author
-        """
-        try:
-            self.courses.remove(course)
-        except:
-            return False
-
-        return True
-
-    def fillCourses(self):
-        """
-         Fill the list of courses from the ideal term.
-        @return bool :
-        @author
-        """
-        courses = []
-        cursor = MySQLConnection()
-
-        query = 'SELECT idCourse from rel_course_curriculum WHERE idCurriculum = ' + str(self.idCurriculum) + ' AND term = ' + str(self.term)
-        courses_data = cursor.execute(query)
-
-        for course_data in courses_data:
-            courses.append(Course.pickById(course_data[0]))
-
-        self.addCourses(courses)
-        return True
-        
     @staticmethod 
     def find(**kwargs):
         """
@@ -155,7 +98,7 @@ class IdealTerm(object):
          
          A list of objects that match the specifications made by one (or more) of the
          folowing parameters:
-         > idCurriculum
+         > idCycle
          > term
          > courses
          > requisitionType
@@ -172,82 +115,47 @@ class IdealTerm(object):
         @author
         """
         cursor = MySQLConnection()
-        new_kwargs = {}
-        for key in kwargs:
-           if not key == "courses":
-               new_kwargs[key] = kwargs[key]
-        query = 'SELECT DISTINCT idCurriculum, term, startDate, endDate, requisitionType'
-        if kwargs.has_key('requisitionType'): 
-            query += ', requisitionType'
-        if kwargs.has_key('startDate'): 
-            query += ', startDate'
-        if kwargs.has_key('endDate'):
-            query += ', endDate'
-        if kwargs.has_key('courses'): 
-            new_courses = []
-            for course in kwargs['courses']:
-                new_courses.append(course.idCourse)
-            new_kwargs["idCourse"] = new_courses
-            query += ', idCourse'
-        query += ' FROM rel_course_curriculum'
-        objects_to_create = []
-        complements = []
-        number_objects = 0
+        searchData = cursor.find('SELECT idCourse, idCycle, startDate, endDate, term, requisitionType FROM rel_course_cycle ', kwargs)
         idealTerms = []
-        query_result = cursor.find(query, new_kwargs)
-        if len(query_result) > 0:
-            for idealTerms_data in query_result:
-                #if it's the first time that query_result is being read; or 
-                #if the idCurriculum of the current object is different from the current line in the query_result; or
-                #if the term of the current object is different from the current line in the query_result; then
-                #another object must be created        
-                if number_objects == 0 or objects_to_create[number_objects-1][0] != idealTerms_data[0] or objects_to_create[number_objects-1][1] != idealTerms_data[1]:
-                    objects_to_create.append(list(idealTerms_data))
-                    number_objects += 1
-            for idealTerm_data in objects_to_create:
-                idealTerm = IdealTerm(idealTerm_data[0], idealTerm_data[1], idealTerm_data[2].isoformat())
-                idealTerm.fillCourses()
-                if idealTerm_data[3] != None:
-                    endDate = idealTerm_data[3].isoformat()
-                    idealTerm.setEndDate(endDate)
-                idealTerm.requisitionType = idealTerm_data[4]
-                idealTerms.append(idealTerm)
+        for idealTermData in searchData:
+            newIdealTerm = IdealTerm(idealTermData[1], idealTermData[4], idealTermData[2].isoformat(), idealTermData[5], Course.pickById(idealTermData[0]))
+            newIdealTerm.setEndDate(idealTermData[3].isoformat())
+            idealTerms.append(newIdealTerm)
         return idealTerms
 
     def store(self):
         """
-         Creates or alters rel_course_curriculum in the database and returns True if it
+         Creates or alters rel_course_cycle in the database and returns True if it
          works, and False if it does not work.
 
         @return bool :
         @author
         """
-
-        if self.requisitionType == None:
-            requisitionType = 1
-            self.requisitionType = requisitionType
-        else:
-            requisitionType = self.requisitionType
-
         cursor = MySQLConnection()
-
+        if not self.idCycle or not self.idCourse or not self.endDate or not self.startDate or not self.term:
+            return False
+        idCycle = str(self.idCycle)
+        idCourse = str(self.idCourse)
+        term = str(self.term)
+        if self.requisitionType:
+            requisitionType = str(self.requisitionType)
+        else:
+            requisitionType = 'NULL'
+        querySelect = 'SELECT idCourse, idCycle, endDate FROM rel_course_cycle WHERE idCourse = ' + idCourse + ' and idCycle = ' + idCycle + ' and endDate = ' + self.endDate
+        queryInsert = 'INSERT INTO rel_course_cycle (idCourse, idCycle, startDate, endDate, term, requisitionType) values (' + idCourse + ', ' + idCycle + ', ' + self.startDate + ', ' + self.endDate + ', ' + term + ', ' + requisitionType + ')'
+        queryUpdate = 'UPDATE rel_course_cycle SET startDate = ' + self.startDate + ', term = ' + term + ', requisitionType = ' + requisitionType ' WHERE idCourse = ' + idCourse + ' and idCycle = ' + idCycle + ' and endDate = ' + self.endDate
         try:
-            for course in self.courses:
-                #Search for idIdealTerm
-                possibleIdealTerms = self.find(idCurriculum = self.idCurriculum, courses = [course], term = self.term, startDate_equal = self.startDate)
-                if len(possibleIdealTerms) > 0:
-                    if not self.startDate == possibleIdealTerms[0].startDate or not self.endDate == possibleIdealTerms[0].endDate:
-                        query = "UPDATE rel_course_curriculum SET startDate = '" + self.startDate + "', endDate = '" + self.endDate + "' WHERE idCourse = " + str(course.idCourse) + ' AND idCurriculum = ' + str(self.idCurriculum)
-                        cursor.execute(query)
-                        cursor.commit()
-
-                else:
-                    #If there is no idIdealTerm create row
-                    query = 'INSERT INTO rel_course_curriculum (idCourse, idCurriculum, startDate, endDate, term, requisitionType) VALUES(' + str(course.idCourse) + ', ' + str(self.idCurriculum) + ", '" + self.startDate + "', '" + self.endDate + "', " + str(self.term)+ ', ' + str(requisitionType) + ')'
-                    cursor.execute(query)
-                    cursor.commit()
+            searchData = cursor.execute(querySelect)
+            if not searchData:
+                cursor.execute(queryInsert)
+                cursor.commit()
+            else:
+                cursor.execute(queryUpdate)
+                cursor.commit()
         except:
-            raise IdealTermError('Error adding or updating IdealTerm')
+            return False
+        return True
+    
 
     def __eq__(self, other):
         if not isinstance(other, IdealTerm):
@@ -264,14 +172,15 @@ class IdealTerm(object):
         @author
         """
         cursor = MySQLConnection()
-        if not self.courses:
-            raise IdealTermError('No courses in this IdealTerm')
-        for course in self.courses:
-            query = 'delete from rel_course_curriculum where idCurriculum = ' + str(self.idCurriculum) + ' and term = ' + str(self.term) + " and startDate = '" + self.startDate + "' and endDate = '" + self.endDate + "' and idCourse = " + str(course.idCourse)
-            if self.requisitionType:
-                query += ' and requisitionType = ' + str(self.requisitionType)
-            try:
-                cursor.execute(query)
-                cursor.commit()
-            except:
-                raise IdealTermError('Error on deleting IdealTerm')
+        if not self.idCourse or not self.idCycle or not self.endDate:
+            raise IdealTermError("Can't uniquely identify object, can't delete database tuple")
+            return False
+        query = 'DELETE FROM rel_course_cycle WHERE idCourse = ' + str(self.idCourse) + ' and idCycle = ' + str(self.idCycle) + ' and endDate = ' + self.endDate
+        try:
+            cursor.execute(query)
+            cursor.commit()
+        except:
+            raise IdealTermError("Couldn't delete object")
+            return False
+        return True
+

202c3b50ac53d2fa7da7c4b5e9cef9ea96dbfb05
Lucas Ludewigs <lucasludewigs@hotmail.com>
2013-07-11 14:00:43 -0300

    Changes in Question.py

--- Question.py
+++ Question.py
@@ -1,5 +1,5 @@
-#coding: utf8
-#from AnswerType import *
+# -*- coding: utf8 -*-
+from AnswerType import *
 from tools.MySQLConnection import MySQLConnection
 
 class QuestionError(Exception):
@@ -14,7 +14,7 @@ class QuestionError(Exception):
 class Question(object):
 
     """
-     
+     Class that represents a question used in an assessment.
 
     :version:
     :author:
@@ -22,33 +22,37 @@ class Question(object):
 
     """ ATTRIBUTES
 
-     Question's written statement
-     
-     o enunciado da pergunta
+     Associated database key.
 
-    statement  (private)
+    idQuestion  (public)
 
-     
+     Question's written statement.
 
-    idQuestion  (private)
+    questionWording  (public)
 
-     
+     The type of the question, that relates it to five possible choice answers.
 
-    answerType  (private)
+    answerType  (public)
 
     """
 
-    def __init__(self, statement, answerType):
+    def __init__(self, questionWording, answerType):
         """
-         
+         Constructor method.
 
-        @param string statement : 
-        @param AnswerType answerType : 
+        @param string questionWording : The question's written questionWording.
+        @param AnswerType answerType : The type of the question, that relates it to five possible choice answers.
         @return  :
         @author
         """
-        self.statement = statement
+	#Parameter verification
+	if not isinstance(answerType,AnswerType) or not AnswerType.pickById(answerType.idAnswerType) == answerType:
+	    raise QuestionError('Parameter answerType must be a AnswerType object that exists in the database.')
+	#Setting parameters that have set function
+        self.setQuestionWording(questionWording)
+	#Setting other parameters
         self.answerType = answerType
+	#Setting None parameters
         self.idQuestion = None
         
     def __eq__(self, other):
@@ -56,7 +60,7 @@ class Question(object):
          Comparison method that returns True if two objects of the class Question are
          equal.
 
-        @param AnswerType other : Other object of the class Question to be compared with a present object.
+        @param Question other : Other object of the class Question to be compared with a present object.
         @return bool :
         @author
         """
@@ -66,10 +70,10 @@ class Question(object):
 
     def __ne__(self, other):
         """
-         Comparison method that returns True if two objects of the class Question are
+         Comparison method that returns True if two objects of the class AnswerType are
          not equal.
 
-        @param AnswerType other : Other object of the class Question to be compared with a present object.
+        @param Question other : Other object of the class AnswerType to be compared with a present object.
         @return bool :
         @author
         """
@@ -78,11 +82,9 @@ class Question(object):
     @staticmethod
     def pickById(idQuestion):
         """
-         returns a Question object once given an idQuestion
-         
-         retorna um objeto Pergunta a partir do idPergunta
+         Returns a Question object specified by its database ID.
 
-        @param int idQuestion : 
+        @param int idQuestion : Database ID.
         @return Question :
         @author
         """
@@ -92,23 +94,22 @@ class Question(object):
             questionData = cursor.execute("""SELECT idQuestion, questionWording, idAnswerType FROM question WHERE idQuestion = """ + str(idQuestion))[0]
         except:
             return None
-        question = Question(questionData[1], questionData[2])
+        question = Question(questionData[1], AnswerType.pickById(questionData[2]))
         question.idQuestion = questionData[0]
         return question
 
-    def setStatement(self, newStatement):
+    def setQuestionWording(self, newQuestionWording):
         """
-         
+         Changes the question questionWording.
 
-        @param string newStatement : novo enunciado para a pergunta
+        @param string questionWording : New questionWording for the question.
         @return  :
         @author
         """
-        #Checked, is OK   
-        if not isinstance(newStatement, str):
-            return False
-        self.statement = newStatement
-        return True
+        #Checked, is OK
+        if not isinstance(newQuestionWording,(unicode,str)):
+            raise QuestionError('Parameter newQuestionWording must be unicode or string.')
+        self.questionWording = newQuestionWording
 
     @staticmethod
     def find(**kwargs):
@@ -116,40 +117,41 @@ class Question(object):
          Searches the database to find one or more objects that fit the description
          specified by the method's parameters. It is possible to perform two kinds of
          search when passing a string as a parameter: a search for the exact string
-         (EQUAL operator) and a search for at least part of the string (LIKE operator). 
+         (EQUAL operator) and a search for at least part of the string (LIKE operator).
+         
+         Returns:
+         All the objects that are related to existing questions in the database, if there
+         are not any parameters passed.
+         
+         A list of objects that match the specifications made by one (or more) of the
+         folowing parameters:
+         > idQuestion
+         > questionWording_equal or questionWording_like
+         > category_equal or category_like
+         The parameters must be identified by their names when the method is called, and
+         those which are strings must be followed by "_like" or by "_equal", in order to
+         determine the kind of search to be done.
+         E. g. Question.find(questionWording_like = "How many", category_equal = "Hour")
 
-        Returns:
-        All the objects that are related to existing offers in the database, if there
-        are not any parameters passed.
-
-        A list of objects that match the specifications made by one (or more) of the
-        folowing parameters:
-        > idQuestion
-        > statement_equal or statement_like
-        > category_equal or category_like
-        The parameters must be identified by their names when the method is called, and
-        those which are strings must be followed by "_like" or by "_equal", in order to
-        determine the kind of search to be done.
-        E. g. Question.find(statement_like = "How many", category_equal = "Hour")
+        @param {} _kwargs : Dictionary of arguments to be used as parameters for the search.
+        @return  :
+        @author
         """
         cursor = MySQLConnection()
         questionsData = cursor.find("""SELECT idQuestion, questionWording, idAnswerType FROM question""",kwargs)
         questions = []
         for questionData in questionsData:
-            question = Question(questionData[1], questionData[2])
+            question = Question(questionData[1], AnswerType.pickById(questionData[2]))
             question.idQuestion = questionData[0]
             questions.append(question)
         return questions
 
     def store(self):
         """
-         adds object to database if it does not exist on the table or changes it if it
-         does. Returns "true" if object is stored and "false" if it isn't.
-         
-         insere no banco caso o objeto não exista na tabela ou altera, caso contrário.
-         Retorna true caso o objeto tenha sido armazenado ou false, caso contrário
+         Adds object to database if it does not exist on the table or changes it if it
+         does.
 
-        @return bool :
+        @return  :
         @author
         """
         cursor = MySQLConnection()
@@ -157,33 +159,30 @@ class Question(object):
         if self.idQuestion:
             #Yes, the question exists
             #We will update its data
-            query = """UPDATE question SET idAnswerType = """ +str(self.idAnswerType) +""", questionWording = '""" +self.statement + """'"""
+            query = """UPDATE question SET idAnswerType = """ +str(self.idAnswerType) +""", questionWording = '""" +self.questionWording + """'"""
             query += """ WHERE idQuestion = """ +str(self.idQuestion)
         else:
             #No, the question does not exist
             #Is there one just like it?
-            possibleQuestions = Question.find(statement_equal = self.statement, category_equal = self.category)
+            possibleQuestions = Question.find(questionWording_equal = self.questionWording, answerType = self.answerType)
             if len(possibleQuestions) > 0:
                 #There's one just like it!
                 self.idQuestion = possibleQuestions[0].idQuestion
                 return None
+	    else:
                 #No, let's create it
-                query = """INSERT INTO question (idAnswerType, questionWording) VALUES """
-                query += str((self.idAnswerType, """'"""+self.statement+"""'""")
+                query = "INSERT INTO question (idAnswerType, questionWording) VALUES ( "
+                query += str(self.answerType.idAnswerType) + ",'" + str(self.questionWording) + "')"
         #Execute query
         cursor.execute(query)
         cursor.commit()
         return None
-        
 
-    def remove(self):
+    def delete(self):
         """
-         removes object from database. Returns "true" if succeeds
-         
-         remove o objeto do banco de dados e retorna True se conseguir, caso contrario
-         retorna False
+         Deletes a question from the database.
 
-        @return bool :
+        @return  :
         @author
         """
         
@@ -194,9 +193,9 @@ class Question(object):
                 cursor.execute("""DELETE FROM rel_question_questionnaire WHERE idQuestion = """ + str(self.idQuestion))
                 cursor.commit()
             else:
-                raise QuestionError("Can't delete non saved object.")
+                raise QuestionError("Can't delete non saved question.")
         else:
-            raise CourseError('idQuestion not defined.')
+            raise QuestionError('idQuestion not defined.')
 
 
 
